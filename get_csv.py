5276202d903262434fd7a8764d5207220a1d3de9
TomboyLU <93912097+TomboyLU@users.noreply.github.com>
2024-05-06 13:11:11 +0800

    Update get_csv.py

--- get_csv.py
+++ get_csv.py
@@ -2,9 +2,7 @@ import csv
 from glob import glob
 from tqdm import tqdm
 import os
-
 import argparse
-
 parser = argparse.ArgumentParser(description='Process some paths.')
 
 parser.add_argument('--dataset_path', type=str, default='./StyleGAN/train/',)
@@ -12,6 +10,7 @@ parser.add_argument('--dataset_path', type=str, default='./StyleGAN/train/',)
 parser.add_argument('--save_csv_path', type=str, default='./StyleGAN/train.csv')
 
 args = parser.parse_args()
+path = args.dataset_path
 
 path_list = [0, 1]
 headers = ['filename', 'label']
@@ -20,10 +19,10 @@ with open(args.save_csv_path, 'w') as f:
     writer = csv.writer(f)
     writer.writerow(headers)
     for i in tqdm(range(len(path_list))):
-        file_path = args.path + '/' + str(i)
+        file_path = path + '/' + str(i)
         file_list = sorted(glob(file_path + '/*.png'))
         for j in range(len(file_list)):
-            tmp.append(os.args.path.basename(file_list[j]))
+            tmp.append(os.path.basename(file_list[j]))
             tmp.append(i)
             writer.writerow(tmp)
             tmp = []
